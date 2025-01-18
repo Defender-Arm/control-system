189a0995f4a3ed4f9d62a7707aae396c507275fc
gavboi <50150638+gavboi@users.noreply.github.com>
2025-01-18 17:25:43 -0500

    interface implemented

---
+++ src/backend/sensor_fusion/tracking.py
@@ -0,0 +1,47 @@
+from numpy import ndarray
+from typing import List, Tuple
+
+
+_history = []
+
+
+def find_in_image(image: ndarray) -> Tuple[int, int]:
+    """Finds the pixel coordinates of the centre of the object in the image.
+    :return: Pixel location, (x, y) from top left
+    """
+    raise NotImplementedError
+
+
+def create_ray(pixel_x: int, pixel_y: int) -> Tuple[float, float, float]:
+    """Calculates angles for line in real space which passes from the center of
+    the camera through the object in the image.
+    :returns: Angles in radians along x-axis, then y-axis, then z-axis
+    """
+    raise NotImplementedError
+
+
+def locate_object(
+        left_ray_angles: Tuple[float, float, float],
+        right_ray_angles: Tuple[float, float, float]
+) -> Tuple[float, float, float]:
+    """Finds where two rays intersect or are the closest to intercepting.
+    :raise RuntimeError: Rays do not intersect and shortest distance between them is greater than 0.1 metre
+    :return: Metres from base joint; x-axis, then y-axis, then z-axis
+    """
+    raise RuntimeError
+
+
+def store_location(timestamp: float, location: Tuple[float, float, float]) -> None:
+    """Adds the location at its timestamp to the stored history. Stores most recent 15 entries.
+    """
+    if len(_history) == 15:
+        _history.pop()
+    _history.insert(0, (timestamp, location))
+    return
+
+
+def get_location_history() -> List[Tuple[float, Tuple[float, float, float]]]:
+    """Returns the last 15 locations and when it was detected.
+    :return: list of timestamp and (x,y,z) metre coordinate sets
+    """
+    return _history
