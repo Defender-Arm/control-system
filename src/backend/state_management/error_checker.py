df980090778e6f162a5577be3976c5214e92f068
gavboi <50150638+gavboi@users.noreply.github.com>
2025-01-29 19:18:02 -0500

    implemented logic and added basic test file

--- src/backend/state_management/error_checker.py
+++ src/backend/state_management/error_checker.py
@@ -1,25 +1,38 @@
 from typing import List, Tuple
 
 
-def verify_track(history: Tuple[float, Tuple[float, float, float]]) -> bool:
+MAX_TIME = 0.2  # s
+MAX_DIST = 1  # m
+
+
+def verify_track(history: List[Tuple[float, Tuple[float, float, float]]]) -> bool:
     """Verifies inputs for error cases.
     :return: If track is valid
     """
-    # verify_track: calls all other functions and returns logical or of their return values.
-    raise NotImplementedError
+    timestamps = [frame[0] for frame in history]
+    locations = [frame[1] for frame in history]
+    return _verify_time(timestamps) and _verify_location(locations)
 
 
 def _verify_time(timestamps: List[float]) -> bool:
     """Ensures time frame between tracking updates is reasonable.
     :return: If timestamps are valid
     """
-    # verify_time: returns true if any two adjacent timestamps are more than 200 milliseconds apart.
-    raise NotImplementedError
+    for i in range(len(timestamps) - 1):
+        if abs(timestamps[i] - timestamps[i+1]) > MAX_TIME:
+            return False
+    return True
 
 
 def _verify_location(locations: List[Tuple[float, float, float]]) -> bool:
     """Ensures locations at each time frame are a realistic distance apart.
     :return: If locations are valid
     """
-    # verify_location: returns true if any two adjacent locations are more than 1 metre apart.
-    raise NotImplementedError
+    for i in range(len(locations) - 1):
+        if (
+                (locations[i][0] - locations[i+1][0])**2 +
+                (locations[i][1] - locations[i+1][1])**2 +
+                (locations[i][2] - locations[i+1][2])**2
+        ) ** 0.5 > MAX_DIST:
+            return False
+    return True
