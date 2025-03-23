70da616c84eadc608c3c451d11ce9e5e81b6bddd
gavboi <50150638+gavboi@users.noreply.github.com>
2025-03-23 14:21:23 -0400

    fixed boundary error, added logging and jitter protection

--- src/test/external_management/connections.py
+++ src/test/external_management/connections.py
@@ -43,18 +43,23 @@ def find_red_stick_center_orientation(image):
         return None, None, None, None
 
 # Example usage with webcam
-cap = cv2.VideoCapture(0)  # Open webcam
+cap = cv2.VideoCapture(1)  # Open webcam
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 last = 0
+last_ang = [999, 999, 999]
 half_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) // 2
 half_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) // 2
-ser = serial.Serial('COM4', 9600, timeout=1)
+ser = None
+#ser = serial.Serial('COM4', 9600, timeout=1)
 def send_command(base, elbow, wrist):
+    if ser is None:
+        return
     command = f"{base} {elbow} {wrist}\n"
     ser.write(command.encode('utf-8'))
     print(f"Sent: {command.strip()}")
 print(f'{half_width*2}x{half_height*2}')
+logfile = open('connections_test.log', 'w')
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -75,34 +80,50 @@ while True:
 
     cv2.imshow("Frame", mask)
 
-    if last + 0.5 < monotonic():
-        last = monotonic()
-        target_x = (center[0] - half_width) / half_width * 60
-        target_y = (center[1] - half_height) / half_height * 45
-        if target_x < -60:
-            print(f'target_x increased from {target_x} to -60')
-            target_x = -60
-        if target_x > 60:
-            print(f'target_x decreased from {target_x} to 60')
-            target_x = 60
-        if target_y < -45:
-            print(f'target_y increased from {target_y} to -45')
-            target_x = -45
-        if target_x > 45:
-            print(f'target_y decreased from {target_y} to 45')
-            target_x = 45
-        print(f'target is {int(target_x)} {-int(target_y)}')
-        target_wr = angle
-        if abs(target_wr) > 90:
-            print(f'target_wr changed from {target_wr} to 90')
-            target_wr = 90
-        send_command(int(target_x), -int(target_y), int(target_wr))
+    target_x = (center[0] - half_width) / half_width * 60
+    target_y = (center[1] - half_height) / half_height * 45
+    if target_x < -60:
+        print(f'target_x increased from {target_x} to -60')
+        target_x = -60
+    if target_x > 60:
+        print(f'target_x decreased from {target_x} to 60')
+        target_x = 60
+    if target_y < -45:
+        print(f'target_y increased from {target_y} to -45')
+        target_x = -45
+    if target_y > 45:
+        print(f'target_y decreased from {target_y} to 45')
+        target_x = 45
+    target_wr = angle
+    if abs(target_wr) > 90:
+        print(f'target_wr changed from {target_wr} to 90')
+        target_wr = 90
+    if last + 0.2 < monotonic():
+        if (
+                abs(last_ang[0] - target_x) > 5 or
+                abs(last_ang[1] - target_y) > 5 or
+                abs(last_ang[2] - target_wr) > 5
+        ):
+            print(f'target is {int(target_x)} {-int(target_y)} {int(target_wr)}')
+            last = monotonic()
+            last_ang = [target_x, target_y, target_wr]
+            send_command(int(target_x), -int(target_y), int(target_wr))
+            logfile.write(f'o {int(target_x)} {-int(target_y)} {int(target_wr)}\n')
+        else:
+            last = monotonic()
+            print(f'RESEND target is {int(last_ang[0])} {-int(last_ang[1])} {int(last_ang[2])}')
+            send_command(int(last_ang[0]), -int(last_ang[1]), int(last_ang[2]))
+            logfile.write(f'r {int(last_ang[0])} {-int(last_ang[1])} {int(last_ang[2])}\n')
+    else:
+        logfile.write(f'x {int(target_x)} {-int(target_y)} {int(target_wr)}\n')
 
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
         break
 
 cap.release()
 cv2.destroyAllWindows()
+logfile.close()
 send_command(0,0,0)
 sleep(3)
-ser.close()
+if ser is not None:
+    ser.close()
