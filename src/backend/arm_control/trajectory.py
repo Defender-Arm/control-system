1045e3c2c44c6a02a8e2eb2a7a33d339c4712c75
gavboi <50150638+gavboi@users.noreply.github.com>
2025-04-04 15:30:54 -0400

    added func signatures, used simple trajectory calculation before arm block

---
+++ src/backend/arm_control/trajectory.py
@@ -0,0 +1,25 @@
+from typing import List, Tuple
+import numpy
+
+
+def calculate_trajectory(history: List[Tuple[float, numpy.typing.NDArray[numpy.float64]]]):
+    """Using past locations of object, predict parabola of sword swing.
+    :return: Trajectory of estimated swing arc
+    """
+    raise NotImplementedError
+
+
+def calculate_collision() -> numpy.typing.NDArray[numpy.float64]:
+    """Calculate interception point of line and arm workspace.
+    :return: intercept as 3D point
+    """
+    raise NotImplementedError
+
+
+def simple_trajectory(location: numpy.typing.NDArray[numpy.float64]) -> numpy.typing.NDArray[numpy.float64]:
+    """Exaggerates position of object to force larger response from arm.
+    :return: New exaggerated location
+    """
+    location[0] = location[0] * 2
+    location[2] = location[2] * 1.5
+    return location
